eacb49c3252a02bca1a4292cb9b9105c010f1102
AshiqueBourg <ashique@bourgeontech.com>
2026-01-19 22:18:58 +0530

    web icons

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,15 +8,48 @@
 
             <nav class="main-nav">
                 <ul>
-                    <li><a href="{{ route('home') }}" class="active">Home</a></li>
-                    <li><a href="#industries">Industries</a></li>
-                    <li><a href="{{ route('portfolio') }}">Portfolio</a></li>
-                    <li><a href="{{ route('case.studies') }}">Case Studies</a></li>
-                    <li><a href="#pricing">Pricing</a></li>
-                    <li><a href="{{ route('about.us') }}">About Us</a></li>
-                    <li><a href="#contact">Contact Us</a></li>
+                    <li>
+                        <a href="{{ route('home') }}" class="{{ request()->routeIs('home') ? 'active' : '' }}">
+                            Home
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#industries" class="{{ request()->is('/') ? '' : '' }}">
+                            Industries
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="{{ route('portfolio') }}"
+                            class="{{ request()->routeIs('portfolio*') ? 'active' : '' }}">
+                            Portfolio
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="{{ route('case.studies') }}"
+                            class="{{ request()->routeIs('case.studies*') ? 'active' : '' }}">
+                            Case Studies
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#pricing">Pricing</a>
+                    </li>
+
+                    <li>
+                        <a href="{{ route('about.us') }}" class="{{ request()->routeIs('about.us*') ? 'active' : '' }}">
+                            About Us
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#contact">Contact Us</a>
+                    </li>
                 </ul>
             </nav>
+
 
             <a href="#contact" class="btn btn-sm btn-accent header-cta">Get Started</a>
 
